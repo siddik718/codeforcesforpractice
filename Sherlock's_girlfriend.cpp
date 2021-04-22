4a5709d191aec0aac888219c54be1d217738e6ce
Abu Bakar Siddik <70488808+siddik718@users.noreply.github.com>
2021-04-22 23:20:38 +0600

    Add files via upload

---
+++ Sherlock's_girlfriend.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define N 100000
+using namespace std;
+vector <bool> ar(N+5,true);
+void sieve(int n){
+for(int i = 2;i*i<=n+1;i++){
+    if(ar[i]){
+        for(int j = 2*i;j<=n+1;j+=i)
+            ar[j] = false;
+    }
+}
+}
+int main(){
+int n;
+cin >> n;
+   sieve(n);
+if(n>2)
+cout << "2\n";
+else
+cout << "1\n";
+for(int i = 2;i<=n+1;i++){
+    if(ar[i])
+        cout << "1 ";
+    else
+        cout << "2 ";
+}
+return 0;
+}
+
